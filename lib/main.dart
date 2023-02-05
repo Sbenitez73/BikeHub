6533261c28e69dfbefdc9d454e0ea261af551c6f
Santiago Benitez <sbenitez@sbenitezdev-mac.local>
2023-02-05 17:59:56 -0500

    first commit!

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:bike_hub/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+          fontFamily: 'Poppins',
+        ),
+        primarySwatch: Colors.blue,
+      ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        AppLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('es'),
+      ],
+      home: const LoginScreen(),
+    );
+  }
+}
